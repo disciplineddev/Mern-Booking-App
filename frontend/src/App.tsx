6529271ea5dc4e1305ee1b39926dbab2414467df
disciplineddev <disciplineddev1994@gmail.com>
2024-05-07 22:09:07 +0530

    Added Header, Footer and Register Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,22 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout></Layout>} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search Page</p>
+            </Layout>
+          }
+        />
       </Routes>
     </Router>
   );
