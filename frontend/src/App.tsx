56f8ea2561b598db93bc94faaf62b832cfb97681
disciplineddev <disciplineddev1994@gmail.com>
2024-06-09 21:14:56 +0530

    added my hotels view and added my hotels e2e test cases

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
+import MyHotels from "./pages/MyHotels";
 
 function App() {
   const { isLoggedIn } = useAppContext();
@@ -49,14 +50,24 @@ function App() {
           }
         />
         {isLoggedIn && (
-          <Route
-            path="/add-hotel"
-            element={
-              <Layout>
-                <AddHotel />
-              </Layout>
-            }
-          />
+          <>
+            <Route
+              path="/add-hotel"
+              element={
+                <Layout>
+                  <AddHotel />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-hotels"
+              element={
+                <Layout>
+                  <MyHotels />
+                </Layout>
+              }
+            />
+          </>
         )}
 
         <Route path="*" element={<Navigate to="/" />} />
